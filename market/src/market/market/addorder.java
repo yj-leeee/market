7a6e14480cf22e6ce193ee29ace24eba9bdbe912
yj-leeee <asas87189@gmail.com>
2025-06-18 15:16:34 +0900

    메뉴다시보기 기능 추가

--- market/src/market/market/addorder.java
+++ market/src/market/market/addorder.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class addorder {
     public static boolean askAddorder(Scanner scanner) {
         while (true) {
-            System.out.print("추가 주문을 하시겠습니까? (Y/N): ");
+            System.out.print("다시 보고싶으시면 '다시보기'라고 입력해주세요.\n추가 주문을 하시겠습니까? (Y/N): ");
             String answer = scanner.nextLine().trim().toUpperCase();
 
             if (answer.equals("Y")) {
@@ -13,7 +13,11 @@ public class addorder {
             } else if (answer.equals("N")) {
 //                System.out.println("결제창으로 넘어갑니다.");
                 return false;
-            } else {
+            } else if (answer.equals("다시보기")) {
+            	menu.menu();
+            	return true;
+            }
+            else {
                 System.out.println("잘못된 입력입니다.");
             }
         }
