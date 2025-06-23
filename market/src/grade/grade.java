bfdbb14b8445994a550d4ce4fb1ae864d17c10ca
yj-leeee <asas87189@gmail.com>
2025-06-23 14:29:02 +0900

    grade 클래스 다 작성

--- market/src/grade/grade.java
+++ market/src/grade/grade.java
@@ -1,5 +1,51 @@
 package grade;
 
 public class Grade {
-
+	private String name;
+	private String grade; 
+	private double bonusRate;
+	private int point;
+	private int totalSpent;
+	
+	public Grade(String name) {
+		this.name = name;
+		this.grade = "실버";
+		this.bonusRate = 0.01;
+		this.point = 0;
+		this.totalSpent = 0;
+	}
+	
+	//구매 금액 처리
+	public void purchase(int amount) {
+		totalSpent += amount;
+		point += amount * bonusRate;
+		
+		updateGrade(); //누적 금액 기반 등급 재설정
+	}
+	
+	private void updateGrade() {
+		if(totalSpent >= 100000 && !grade.equals("골드")) {
+			grade = "골드";
+			bonusRate = 0.05;
+			System.out.println(name + "님의 등급이 골드로 상승했습니다.");
+		}else if(totalSpent >= 150000 && !grade.equals("실버")) {
+			grade = "다이아";
+			bonusRate = 0.10;
+			System.out.println(name + "님의 등급이 다이아로 상승했습니다.");
+			
+		}
+	}
+	
+	public String getGrade() {
+		return grade;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public int getTotalSpent() {
+		return totalSpent;
+	}
+	public String getName() {
+		return name;
+	}
 }
