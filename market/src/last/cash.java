2a1138f5d21384d436db1eccff93e5a1e6a3633b
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-06-27 13:12:18 +0900

    기존유저 적립금 500원까지

--- market/src/last/cash.java
+++ market/src/last/cash.java
@@ -12,11 +12,17 @@ public class cash {
 			System.out.println("현재 적립 금액: " + user.getPoint() );
 			System.out.println("더 필요한건 없으십니까? \n1:네 결제하겠습니다. \n2:아니요 주문창으로 다시 돌아가주세요\n답변: ");
 			int answer = scanner.nextInt();
-			
+			scanner.nextLine(); // 버퍼 비우기
 			switch (answer) {
 				case 1 :
 					lastcash.lastcash(user, totalPrice);
-				
+					break;
+				case 2:
+			        System.out.println("주문창으로 돌아갑니다.");
+			        break;
+			    default:
+			        System.out.println("잘못된 입력입니다.");
+			        break;
 					
 					
 			}
